f1631cd9c49b6361bf509d1a9051da7ea31ee53d
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-02-28 00:43:59 +0100

    repository

---
+++ src/main/java/com/nuuedscore/repository/PersonRepository.java
@@ -0,0 +1,23 @@
+package com.nuuedscore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.nuuedscore.domain.Person;
+
+/**
+ * Person Repository
+ * 
+ * @author PATavares
+ * @since Feb 2021
+ * 
+ */
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    @Query("SELECT p FROM Person p WHERE p.username = :usernameToFind")
+    public Person findByUsername(String usernameToFind);
+	
+    @Query("SELECT p FROM Person p WHERE p.email = :emailToFind")
+    public Person findByEmail(String emailToFind);
+
+}

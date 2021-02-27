0b881deb8d7eff5b1454fbf2b986016ac07822df
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-02-28 00:45:44 +0100

    Spring Boot Application

---
+++ src/main/java/com/nuuedscore/NuuEdScoreApplication.java
@@ -0,0 +1,33 @@
+package com.nuuedscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * NuuED SCORE Server Application
+ * 
+ * @author PATavares
+ * @since Feb 2021
+ * 
+ */
+@Slf4j
+@SpringBootApplication
+public class NuuEdScoreApplication {
+
+	public static void main(String[] args) {
+		log.info("NuuED SCORE by JavaLabs - v0.1 BETA");
+		log.info("Reactive AI - While some watched from far behind... Learn as Apply. Refuse to Stay Down.");
+		log.info("user.home @ {}", System.getProperty("user.home"));
+		log.info("user.dir @ {}", System.getProperty("user.dir"));
+		
+		ApplicationContext applicationContext = SpringApplication.run(NuuEdScoreApplication.class, args);
+		
+		// Debug Loaded Beans in Context
+		for (String name: applicationContext.getBeanDefinitionNames()) {
+			log.debug(name);
+		}		
+	}
+}
---
+++ src/test/java/com/nuuedscore/NuuEdScoreApplicationTests.java
@@ -0,0 +1,21 @@
+package com.nuuedscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+
+/**
+ * NuuED SCORE Application Tests   
+ *
+ * @author PATavares
+ * @since Feb 2021
+ * 
+ */
+@SpringBootTest
+class NuuEdScoreApplicationTests {
+
+	@Test
+	@Order(1)
+	void contextLoads() {}
+
+}

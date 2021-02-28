e10ffb2994ea3eeabcf89b9488f2b87a7153acf6
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-02-28 11:46:51 +0100

    Introduce LearningPersonality

--- src/main/java/com/nuuedscore/component/DomainInitializer.java
+++ src/main/java/com/nuuedscore/component/DomainInitializer.java
@@ -9,8 +9,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+import com.nuuedscore.domain.LearningPersonality;
 import com.nuuedscore.domain.Privilege;
 import com.nuuedscore.domain.Role;
+import com.nuuedscore.repository.LearningPersonalityRepository;
 import com.nuuedscore.repository.PrivilegeRepository;
 import com.nuuedscore.repository.RoleRepository;
 
@@ -34,18 +36,42 @@ public class DomainInitializer implements ApplicationContextAware {
 	@Autowired
 	private PrivilegeRepository privilegeRepository;
 
+	@Autowired
+	private LearningPersonalityRepository learningPersonalityRepository;
+	
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {	
 		log.info("Initializing DOMAIN...", this.getClass().getSimpleName());
 
+		/*
+		 * ROLE
+		 */
 		// Security defaults
 		createRoleIfNotFound("ROLE_ADMIN");
 		createRoleIfNotFound("ROLE_USER");
 		// Application
 		createRoleIfNotFound("ROLE_CURATOR");
-		createRoleIfNotFound("ROLE_STUDENT");		
+		createRoleIfNotFound("ROLE_STUDENT");
+		
+		/*
+		 * PRIVILEGE
+		 */
+		
+		/*
+		 * LEARNING PERSONALITY 
+		 */
+		createLearningPersonalityIfNotFound("VISUAL");
+		createLearningPersonalityIfNotFound("AURAL");
+		createLearningPersonalityIfNotFound("VERBAL");
+		createLearningPersonalityIfNotFound("SOCIAL");
+		createLearningPersonalityIfNotFound("LOGICAL");
+		createLearningPersonalityIfNotFound("PHYSICAL");
+		createLearningPersonalityIfNotFound("SOLITARY");
 	}
 
+	/*
+	 * ROLE
+	 */
 	private Role createRoleIfNotFound(String roleName) {
 		return createRoleIfNotFound(roleName, Collections.emptyList()); // no privilege as all privilege for now
 	}
@@ -59,4 +85,22 @@ public class DomainInitializer implements ApplicationContextAware {
 		}
 		return role;
 	}
+	
+	/*
+	 * PRIVILEGE
+	 */
+	//TODO
+	
+	/*
+	 * LEARNING PERSONALITY
+	 */
+	private LearningPersonality createLearningPersonalityIfNotFound(String name) {
+		LearningPersonality learningPersonality = learningPersonalityRepository.findByName(name);
+		if (learningPersonality == null) {
+			learningPersonality = new LearningPersonality(name);
+			learningPersonalityRepository.save(learningPersonality);
+		}
+		return learningPersonality;
+	}
+
 }

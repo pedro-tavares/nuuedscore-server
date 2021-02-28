1bb177f228483b554430417d7a250abc023ed750
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-02-28 23:31:22 +0100

    @author

--- src/main/java/com/nuuedscore/refdata/NuuEdScoreConstants.java
+++ src/main/java/com/nuuedscore/refdata/NuuEdScoreConstants.java
@@ -1,5 +1,12 @@
 package com.nuuedscore.refdata;
 
+/**
+ * NuuEd Score Constants 
+ * 
+ * @author PATavares
+ * @since Feb 2021
+ * 
+ */
 public abstract class NuuEdScoreConstants {
 
 	public static String DEFAULT_PASSWORD = "password";

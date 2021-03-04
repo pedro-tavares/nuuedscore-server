4559562c0cdef7f909dcc7b784ca72142da0519a
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-03-04 12:18:37 +0100

    TeacherResource and Repository

--- src/main/java/com/nuuedscore/domain/TeacherResource.java
+++ src/main/java/com/nuuedscore/domain/TeacherResource.java
@@ -39,15 +39,17 @@ import lombok.extern.slf4j.Slf4j;
 @Entity
 public class TeacherResource extends BaseDomain {
 	
-	private static final long serialVersionUID = -6185731271053302664L;
+	private static final long serialVersionUID = -1713364144496580084L;
 
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 	private String topic;
 	@Enumerated(EnumType.STRING)
+	/*
 	private RefScore score;
 	@Enumerated(EnumType.STRING)
+	*/
 	@Column(name = "learning_personality")
 	private RefLearningPersonality learningPersonality;
 	@Enumerated(EnumType.STRING)
@@ -58,12 +60,17 @@ public class TeacherResource extends BaseDomain {
     @Column(name = "created_on")
     private LocalDateTime createdOn;
 
-    public TeacherResource(String topic, RefScore score, RefLearningPersonality learningPersonality, RefBloom bloom, String subject, String name, String resource) {
+    public TeacherResource(String topic, /*String score,*/ String learningPersonality, String bloom, String subject, String name, String resource) {
+    	this(topic, /*RefScore.get(score),*/ RefLearningPersonality.get(learningPersonality), RefBloom.get(bloom), subject, name, resource);
+    }
+    
+    public TeacherResource(String topic, /*RefScore score,*/ RefLearningPersonality learningPersonality, RefBloom bloom, String subject, String name, String resource) {
     	this.topic = topic;
-    	this.score = score;
+    	//this.score = score;
     	this.learningPersonality = learningPersonality;
     	this.bloom = bloom;
     	this.subject = subject;
+    	this.name = name;
     	this.resource = resource;
     }
     
---
+++ src/main/java/com/nuuedscore/repository/TeacherResourceRepository.java
@@ -0,0 +1,21 @@
+package com.nuuedscore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.nuuedscore.domain.StudentResource;
+import com.nuuedscore.domain.TeacherResource;
+
+/**
+ * Teacher Resource Repository
+ * 
+ * @author PATavares
+ * @since Mar 2021
+ * 
+ */
+public interface TeacherResourceRepository extends JpaRepository<TeacherResource, Long> {
+
+    @Query("SELECT s FROM TeacherResource s WHERE s.resource = :resourceToFind")
+    StudentResource findByResource(String resourceToFind);
+
+}

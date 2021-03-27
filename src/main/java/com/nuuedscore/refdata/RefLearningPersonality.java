3eb65afc1ac30353cf3e5e00357733564e36f87a
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-03-27 21:56:24 +0100

    PHYSICAL to KINESTHETIC

--- src/main/java/com/nuuedscore/refdata/RefLearningPersonality.java
+++ src/main/java/com/nuuedscore/refdata/RefLearningPersonality.java
@@ -16,7 +16,7 @@ public enum RefLearningPersonality {
 	VERBAL("Verbal"),
 	SOCIAL("Social"),
 	LOGICAL("Logical"),
-	PHYSICAL("Physical"),
+	KINESTHETIC("Kinesthetic"),
 	SOLITARY("Solitary"),
 	EGP("EGP");
 

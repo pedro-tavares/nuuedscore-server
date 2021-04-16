2998002314280bf3002c57fc33fd57331a60240d
Pedro Alexandre Tavares <pedro.javalabs@gmail.com>
2021-04-17 00:07:23 +0200

    Evolve Startups

--- src/main/java/com/nuuedscore/component/RAIStartupComponent.java
+++ src/main/java/com/nuuedscore/component/RAIStartupComponent.java
@@ -6,6 +6,9 @@ import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.BeansException;
@@ -14,14 +17,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.yandex.qatools.ashot.AShot;
 import ru.yandex.qatools.ashot.Screenshot;
 import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
- * Domain Startup Component
+ * RAI Startup Component
  *
  * @author PATavares
  * @since Feb 2021
@@ -31,32 +33,63 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class RAIStartupComponent implements ApplicationContextAware {
 
-	protected WebServer driver;
+	protected static WebServer server;
+	protected static WebDriver driver;
 
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		log.info("Starting RAI Site Image capture...", this.getClass().getSimpleName());
 
-		takeScreenShot();
+		setup();
+		
+		//takeScreenShot("https://nuuedscore.com");
+		takeScreenShot("https://www.dragonbox.com");
+		
+		//takeScreenShotWithScroll();
 	}
 
-	public static void takeScreenShot() {
-		WebDriver driver;
-		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
-		driver = new ChromeDriver();
+	public static void setup() {
+		if (driver == null) {
+			System.setProperty("webdriver.chrome.driver", ".\\Driver\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+	}
+	
+	public static void takeScreenShot(String site) {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
-		driver.get("https://nuuedscore.com");
+		driver.get(site);
 		
-		//take screenshot of the entire page             
-		Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(
-				driver);
+		File src;
+		
+		//take screenshot of the page         
+		src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		try {
-			ImageIO.write(screenshot.getImage(), "PNG", new File("screenshots/" + "TEST.png"));
+			FileUtils.copyFile(src, new File("screenshots/" + "screenshot.png"));
 		} catch (IOException e) { 
 			e.printStackTrace();
 		}
+		
+		//driver.quit();
+	}
+
+	public static void takeScreenShotWithScroll(String site) {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		driver.get(site);
+
+		//take screenshot of the entire page             
+		Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(10000)).takeScreenshot(
+				driver);
+
+		try {
+			ImageIO.write(screenshot.getImage(), "PNG", new File("screenshots/" + "TEST_SCROLL.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		driver.quit();
 	}
 }
